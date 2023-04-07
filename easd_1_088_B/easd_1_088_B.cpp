41f7d430dadd16fb67cce9292e0b2944270d3c16
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-04-07 10:40:37 +0700

    jawaban nomor 4

--- easd_1_088_B/easd_1_088_B.cpp
+++ easd_1_088_B/easd_1_088_B.cpp
@@ -7,4 +7,7 @@
 
 // 3. 1. ukuran input, algoritma, struktur data, pegguna sumber daya, bahasa pemograman, lingkungan eksekusi
 
+// 4. Algoritma Merge Sort memecah data menjadi dua bagian secara rekursif, kemudian mengurutkan kedua bagian tersebut, dan akhirnya menggabungkan kedua bagian yang telah diurutkan menjadi satu kesatuan data yang terurut. Algoritma ini memiliki kompleksitas waktu O(n log n), yang membuatnya efisien untuk mengurutkan data yang ukurannya besar.
+
+
 
