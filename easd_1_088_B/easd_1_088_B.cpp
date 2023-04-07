60d6498ec6afdbb545cf585347dc92029fb58ca1
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-04-07 09:44:40 +0700

    jawaban dari Soal nomor 2

--- easd_1_088_B/easd_1_088_B.cpp
+++ easd_1_088_B/easd_1_088_B.cpp
@@ -2,3 +2,6 @@
 // JAWABAN 
 
 // 1. Karena algoritma adalah urutan langkah-langkah terstruktur yang dirancang untuk memecahkan masalah secara sistematis dan efisien.
+
+// 2. Dua sturktur tersebut adalah : Array dan Lingked list
+
