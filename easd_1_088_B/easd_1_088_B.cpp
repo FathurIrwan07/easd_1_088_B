f72fc53b8c21eb51250bef154a873e3140940434
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2023-04-07 10:41:13 +0700

    jawaban nomor 5

--- easd_1_088_B/easd_1_088_B.cpp
+++ easd_1_088_B/easd_1_088_B.cpp
@@ -9,5 +9,8 @@
 
 // 4. Algoritma Merge Sort memecah data menjadi dua bagian secara rekursif, kemudian mengurutkan kedua bagian tersebut, dan akhirnya menggabungkan kedua bagian yang telah diurutkan menjadi satu kesatuan data yang terurut. Algoritma ini memiliki kompleksitas waktu O(n log n), yang membuatnya efisien untuk mengurutkan data yang ukurannya besar.
 
+// 5. Algoritma-algoritma Quadratic: 1. Bubble Sort, 2. Slection Sort, 3. Insertion Sort
+    //Algoritma-algoritma Loglinear: 1. Quick Sort, 2. Marge Sort
 
+/
 
